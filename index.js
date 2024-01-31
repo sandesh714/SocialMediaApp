783f994ef6a8f7196e8e248c6fa90917bedb7e8f
Sandesh Lamsal <sandeshlamsal714@gmail.com>
2024-01-31 07:34:20 -0600

    Some practice

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
+// Define schema
+const Schema = mongoose.Schema;
+
+const SomeModelSchema = new Schema({
+  a_string: String,
+  a_date: Date,
+});
+
+// Compile model from schema
+const SomeModel = mongoose.model("SomeModel", SomeModelSchema);
+
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+  });
+
+  let todos = [];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/api/todos', (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
