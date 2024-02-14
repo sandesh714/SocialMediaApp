28b3b34bfcaec19a3aa843fc9bd8cee4c6db7d29
Sandesh Lamsal <sandeshlamsal714@gmail.com>
2024-02-13 18:44:27 -0600

    Added:SCHEMA, GRAPHQL TYPEDEFS AND RESOLVERS

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
 const dotenv = require("dotenv");
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-// Import the mongoose module
 const mongoose = require("mongoose");
 dotenv.config();
 
 
+const Post = require("./models/Post");
+const { ApolloServer } = require("apollo-server");
+const graphql = require('graphql-tag');
+
+
+const typeDefs = graphql`
+  type Post{
+    id: ID!
+    content: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query{
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts(){
+      try{
+        const posts = await Post.find();
+        return posts;
+      } catch(err){
+        throw new Error(err);
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers
+});
+
+
+
+
+
 
 const connectionString = process.env.MONGODB_SRV;
-const connectToDB = async () => {
-  try {
-      await mongoose.connect(connectionString, {
-          autoIndex: true
-      })
-      console.log('Connected to Mongodb');} catch (error) {
-      console.error(error);
-  }
-}
+mongoose  
+  .connect(connectionString)
+  .then(()=> {
+    console.log("Mongodb connected")
+    return server.listen({ port: 5000});
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
 
-connectToDB();
-
-// Define schema
-const Schema = mongoose.Schema;
-
-const SomeModelSchema = new Schema({
-  a_string: String,
-  a_date: Date,
-});
-
-// Compile model from schema
-const SomeModel = mongoose.model("SomeModel", SomeModelSchema);
-
-
-app.use(bodyParser.json());
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-var { graphql, buildSchema } = require("graphql")
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
-
-// The rootValue provides a resolver function for each API endpoint
-var rootValue = {
-  hello: () => {
-    return "Hello world!"
-  },
-}
-
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue,
-}).then(response => {
-  console.log(response)
-})
